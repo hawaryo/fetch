701bb10ff5d5ecf6fa0249791326dec869515be8
Ahmed <63455817+hawaryo@users.noreply.github.com>
2025-09-20 22:43:37 +0300

    ensure image dimensions are set to prevent layout shift

--- src/app/components/ProductCard.tsx
+++ src/app/components/ProductCard.tsx
@@ -1,13 +1,13 @@
-interface Props  {
+interface Props {
   image: string;
   title: string;
   price: number;
-};
+}
 
 export default function ProductCard({ image, title, price }: Props) {
   return (
     <div className="border p-2">
-      <img src={image} alt={title} />
+      <img src={image} alt={title} width="300" height="300" />
       <h2>{title}</h2>
       <h3>{price}</h3>
     </div>
